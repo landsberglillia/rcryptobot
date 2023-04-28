ce1d780ae4d7861f76f6f3f11815dc35ac8fe4a6
konohaitachi <konoha.itachi0123@gmail.com>
2023-04-28 22:01:26 +0900

    fea

--- fbot/urls.py
+++ fbot/urls.py
@@ -7,6 +7,7 @@ app_name = 'fbot'
 urlpatterns = [
     path("", views.index, name="index"),
     path("register/", views.register, name="register"),
+
     path("registerapp/", views.registerapp, name="registerapp"),
     path('stop1/', views.stopprocess, name='stopprocess'),
     path('test/', views.test, name='test'),
